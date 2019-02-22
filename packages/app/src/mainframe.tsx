59eabb25d60492f3876dfe69352475cc69b50753
tomermes <tomr2005@gmail.com>
2019-02-22 14:06:40 +0200

    implementing drag resizer

--- packages/app/src/mainframe.tsx
+++ packages/app/src/mainframe.tsx
@@ -5,19 +5,85 @@ export interface IMainFrameProps{
     url: string
 }
 
-export default class MainFrame extends React.Component<IMainFrameProps, {} > {
+export interface IMainFrameState{
+    screenSize: number,
+    iframeStyle: {left: string, width: string, pointerEvents: any},
+    resizerStyle: {left: string},
+}
+
+const resizerOffset = 1
+
+export default class MainFrame extends React.Component<IMainFrameProps, IMainFrameState > {
+    private isResizing = false
+
     constructor(props: IMainFrameProps) {
         super(props)
-        this.state = {}
+        this.state = {
+            screenSize: window.innerWidth,
+            iframeStyle: {
+                left: '0px',
+                width: `${window.innerWidth}px`,
+                pointerEvents: 'initial'
+            },
+            resizerStyle: {
+                left: `${resizerOffset}px`
+            }
+        }
+        this._startResizing = this._startResizing.bind(this)
+        this.moveHandler = this.moveHandler.bind(this)
+        this.releaseHandler = this.releaseHandler.bind(this)
     }
 
     public render() {
-        // <iframe src="http://www.wix.com/" />
         return (
-            <div className="mainframe">
+            <div className="mainframe" onMouseMove={this.moveHandler} onMouseUp={this.releaseHandler}>
                 <input defaultValue={this.props.url} />
-                <iframe src={this.props.url} frameBorder={0} />
+                <iframe src={this.props.url} frameBorder={0} style={this.state.iframeStyle}/>
+                <div className="drag-resizer" onMouseDown={this._startResizing} style={this.state.resizerStyle}>
+                    <div className="line" />
+                    <div className="line" />
+                </div>
             </div>
         )
+    }
+
+    public moveHandler(event: React.MouseEvent){
+        if (this.isResizing){
+            this.setState({
+                screenSize: this.state.screenSize - event.movementX * 2
+            })
+            const currLeft = (window.innerWidth - this.state.screenSize) / 2
+            this.setState({
+                iframeStyle: {
+                    left: `${currLeft}px`,
+                    width: `${this.state.screenSize}px`,
+                    pointerEvents: 'none',
+                },
+                resizerStyle: {
+                    left: `${currLeft + resizerOffset}px`
+                }
+            })
+        }
+    }
+
+    public releaseHandler(){
+        if (this.isResizing){
+            this._stopResizing()
+        }
+    }
+
+    public _startResizing(){
+        this.isResizing = true
+    }
+
+    private _stopResizing(){
+        this.isResizing = false
+        this.setState({
+            iframeStyle: {
+                left: this.state.iframeStyle.left,
+                width: this.state.iframeStyle.width,
+                pointerEvents: 'initial',
+            }
+        })
     }
 }
