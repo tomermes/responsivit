64604e75d45d79913a0224a86e8ed3b3a9fcb0c5
tomermes <tomr2005@gmail.com>
2019-02-23 17:06:08 +0200

    adjusting production build script and location

---
+++ packages/app/webpack.production.config.js
@@ -0,0 +1,41 @@
+const {join} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// works great. with the default 'eval', imports are not mapped. inline-source-map also works.
+exports.devtool = 'source-map'
+
+// root of the monorepo, so that paths in output will be clickable
+exports.context = join(__dirname, '..', '..')
+
+exports.entry = join(__dirname, 'src', 'client-bootstrap')
+
+exports.output = {
+    path: join(__dirname, '../.././')
+}
+
+exports.module = {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            loader: '@ts-tools/webpack-loader'
+        },
+        {
+            test: /\.jpg$/,
+            use:[
+                {loader: "file-loader"}
+            ]
+        },
+        {
+            test: /\.css$/, 
+            loader: "style-loader!css-loader",
+        }
+    ]
+}
+
+exports.resolve = {
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+}
+
+exports.plugins = [
+    new HtmlWebpackPlugin({title: 'Responsivit'})
+]
